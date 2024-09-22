b43d34e2d253b33c4f42881010fbc5dc4618d4b5
vijay <vijayandugula9@gmail.com>
2024-09-22 23:09:35 +0530

    variables

--- 11-installation.sh
+++ 11-installation.sh
@@ -8,12 +8,12 @@ then
     echo "please run this script with root priveleges"
 fi
 
-dnf list installed gitttt
+dnf list installed git
 
 if [ $? -ne 0 ]
 then
     echo "Git is not installed, going to install it.."
-    dnf install gitttt -y
+    dnf install git -y
     if [ $? -ne 0 ]
     then
         echo "Git installation is not success...check it"
@@ -23,4 +23,21 @@ then
     fi        
 else
     echo "Git is already installed, Nothing to do.."
-fi        
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed..going to install"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "MYSQL installation is failure..please check"
+        exit 1
+    else
+        echo "MYSQL installation is success"
+    fi
+else
+    echo "MYSQL is already installed..nothing to do"
+fi                
