4348ca71180b2ace2eae5b3d5c22affb4792f8ae
vijay <vijayandugula9@gmail.com>
2024-09-23 14:29:17 +0530

    shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -15,7 +15,7 @@ Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root priveleges $N" &>>$LOG_FILE
+        echo -e "$R Please run this script with root priveleges $N" | tee -a &>>$LOG_FILE
         exit 1
     fi
 }
@@ -23,10 +23,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE | tee -a &>>$LOG_FILE
         exit 1
     else
-        echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE
+        echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE | tee -a &>>$LOG_FILE
     fi
 }
 
@@ -47,13 +47,13 @@ fi
 
 for package in $@ # $@ refers to all arguments passed to it
 do
-    dnf list installed $package &>>$LOG_FILE
+    dnf list installed $package &>>$LOG_FILE | tee -a &>>$LOG_FILE
     if [ $? -ne 0 ]
     then 
-        echo "$package is not installed, going to install it.." &>>$LOG_FILE
-        dnf install $package -y &>>$LOG_FILE
+        echo "$package is not installed, going to install it.." &>>$LOG_FILE | tee -a &>>$LOG_FILE
+        dnf install $package -y &>>$LOG_FILE | tee -a &>>$LOG_FILE
         VALIDATE $? "Installing $package"
     else
-        echo -e "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
+        echo -e "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE | tee -a &>>$LOG_FILE
     fi        
 done  
